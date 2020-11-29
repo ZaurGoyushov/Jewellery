c59abbc75315bd865c638ec2cb652dc85cb6eef1
Zaur Goyushov <zaur.gyshv@gmail.com>
2020-11-29 22:35:38 +0400

    third

--- Shop/Dashboard/routes.py
+++ Shop/Dashboard/routes.py
@@ -1,20 +1,22 @@
 from flask import  render_template, session, request, redirect, url_for,flash
 from Shop import app, db ,bcrypt
 from .forms import RegistrationForm
+from .models import User
 import os
 
 @app.route("/")
 def home():
-    return ('home page')
-
+    return render_template('admin/index.html')
+         
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm(request.form)
     if request.method == 'POST' and form.validate():
-        #user = User(form.username.data, form.email.data,
-                   # form.password.data)
-       # db_session.add(user)
-        flash('Thanks for registering')
-       # return redirect(url_for('register'))
+        Same_password = bcrypt.generate_password_hash(form.password.data)
+        user = User(name=form.name.data, username=form.username.data, email=form.email.data,
+                   password=Same_password)
+        db.session.add(user)
+        flash(f'Welcome{form.name.data}Thanks for registering','success')
+        return redirect(url_for('home'))
     return render_template('admin/register.html', form=form,title='Registration page')
