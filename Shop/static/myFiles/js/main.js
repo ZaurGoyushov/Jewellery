77e8814c3f86487a0b87db491f4dd410e1d63162
Zaur Goyushov <zaur.gyshv@gmail.com>
2020-12-22 02:40:17 +0400

    search layout with js

--- Shop/static/myFiles/js/main.js
+++ Shop/static/myFiles/js/main.js
@@ -1,9 +1,23 @@
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
-    if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
-      document.querySelector(".top_Nav ").style.backgroundColor = "#6c757d";
-    } else {
-      document.querySelector(".top_Nav ").style.backgroundColor = "transparent";
-    }
+  if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
+    document.querySelector(".top_Nav ").style.backgroundColor = "#6c757d";
+  } else {
+    document.querySelector(".top_Nav ").style.backgroundColor = "transparent";
   }
+};
+
+function search() {
+  var srcBtn = document.getElementById('search_layout');
+  srcBtn.style.transform = "scale(1)";
+  srcBtn.style.transformOrigin = "left";
+
+};
+
+function close() {
+  var srcBtn = document.getElementById('search_layout');
+  srcBtn.style.transform = "scale(0)";
+  srcBtn.style.transformOrigin = "right";
+
+}
